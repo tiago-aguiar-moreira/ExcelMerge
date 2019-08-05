87a6fa8d1075b60db41521e3d0473cb5099dd027
Tiago Aguiar Moreira <tiago.aguiar1@hotmail.com>
2019-08-04 21:02:00 -0300

    Implemementei

---
+++ ExcelMerge/Model/MergeProgessFilesModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ExcelMerge.Model
+{
+    public class MergeProgessFilesModel
+    {
+        public int Progress { get; set; }
+
+        public MergeProgessSheetsModel Sheet { get; set; }
+
+        public MergeProgessFilesModel(int lengthSheets)
+        {
+            Progress = 0;
+            Sheet = new MergeProgessSheetsModel(lengthSheets);
+        }
+    }
+}
---
+++ ExcelMerge/Model/MergeProgressSheetsModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ExcelMerge.Model
+{
+    public class MergeProgessSheetsModel
+    {
+        public int Progress { get; set; }
+        public int Total { get; set; }
+        public MergeProgessSheetsModel(int length)
+        {
+            Progress = 0;
+            Total = length;
+        }
+    }   
+}
